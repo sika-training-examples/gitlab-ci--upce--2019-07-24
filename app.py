7e7f30b9d35bc38021007df0182af1b26bd2157c
Ondrej Sika <ondrej@ondrejsika.com>
2019-07-24 12:42:29 +0000

    🦄🦄🦄🦄🦄🦄

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ hostname = os.environ['HOSTNAME']
 @app.route('/')
 def index():
     counter = redis.incr('counter')
-    return 'Awesome counter %s %d\n' % (hostname, counter)
+    return '🦄🦄🦄 Awesome counter %s %d 🦄🦄🦄\n' % (hostname, counter)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='80')
